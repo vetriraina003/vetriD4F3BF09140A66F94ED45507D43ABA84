7521498d2af9c4e2e7a67e55b7a0582a661b6172
vetriraina003 <151503474+vetriraina003@users.noreply.github.com>
2023-11-20 02:02:00 -0500

    Add files via upload

---
+++ challenge 1/main (18).py
@@ -0,0 +1,17 @@
+# Python 3 program to find  
+# factorial of given number 
+
+# Function to find factorial of given number 
+def factorial(n): 
+
+    if n == 0: 
+        return 1
+
+    return n * factorial(n-1) 
+
+# Driver Code 
+num = 5; 
+print("Factorial of", num, "is", 
+factorial(num)) 
+
+# This code is contributed by Smitha Dinesh Semwal 
